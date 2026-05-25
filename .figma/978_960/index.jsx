ec58d66bd03b37091c8bcdb9a821f64af70fa2b1
zzfige-cyber <zzfige@gmail.com>
2026-05-25 11:12:28 +0800

    性能优化

---
+++ .figma/978_960/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <div className={styles.group57}>
+      <div className={styles.frame}>
+        <div className={styles.rectangle93}>
+          <div className={styles.rectangle94} />
+        </div>
+      </div>
+      <p className={styles.pause}>Pause</p>
+    </div>
+  );
+}
+
+export default Component;
